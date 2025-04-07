fda66bacd568c989e97d33c75ada8dc5975aee45
RosariaQ <gyuhyeong135@gmail.com>
2025-04-07 16:51:19 +0900

    colored button

--- flutter_test/flutter_application_1/lib/main.dart
+++ flutter_test/flutter_application_1/lib/main.dart
@@ -38,12 +38,13 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
 
     return Scaffold(
       body: Column(
         children: [
           Text('A random genious idea:'),
-          Text(appState.current.asLowerCase),
+          Bigcard(pair: pair),
 
           ElevatedButton(
             onPressed: (){
@@ -53,6 +54,27 @@ class MyHomePage extends StatelessWidget {
             child: Text('Next'),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class Bigcard extends StatelessWidget {
+  const Bigcard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Card(
+      color:theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(pair.asLowerCase),
       ),
     );
   }
