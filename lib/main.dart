5fdca7f5c5aade2d1f15d9392366a09aa1f0452d
권진우 <jinwoo@Kjw-MacBook-Air.local>
2025-04-25 15:45:16 +0900

    branch_base

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:healthymeal/gptPages/scoreboard.dart';
-import 'package:healthymeal/gptPages/dashboard.dart';
+//import 'package:healthymeal/gptPages/dashboard.dart';
+import 'package:healthymeal/dashboardPage/dashboard.dart';
 
 void main() {
   runApp(const Dashboard());
