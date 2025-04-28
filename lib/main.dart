866bf63e2b07b274fda21d8a0278ac64071c6c25
wist91 <115635370+wist91@users.noreply.github.com>
2025-04-28 15:34:14 +0900

    assets on
    asset 폴더 생성 image files 나눔

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 //import 'package:healthymeal/gptPages/scoreboard.dart';
-//import 'package:healthymeal/gptPages/dashboard.dart';
-import 'package:healthymeal/dashboardPage/dashboard.dart';
+import 'package:healthymeal/gptPages/dashboard.dart';
 
 void main() {
   runApp(const Dashboard());
 }
-
