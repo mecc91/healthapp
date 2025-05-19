51c24abe8bf76169cb15773a3dca732a50a0f4db
wist91 <115635370+wist91@users.noreply.github.com>
2025-05-19 16:13:20 +0900

    폰트 업데이트
    korfont1으로 설정함 전체설정해놧는데 나중에 이상한건 개별로 뺴면될듯

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Healthy Meal',
+      theme: ThemeData(
+        fontFamily: 'korfont1', // ✅ 여기에 커스텀 폰트 적용
+        useMaterial3: true, // 선택사항: Material 3 사용
+      ),
       home: const Dashboard(),
       debugShowCheckedModeBanner: false,
       navigatorObservers: [routeObserver], // ✅ 전역 인스턴스 사용
